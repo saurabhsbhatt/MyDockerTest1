e2b32191b9017e48a91f86fddc1b39132deccf2c
Saurabh Bhatt <sbhatt.84@gmail.com>
2024-08-13 23:29:56 +0530

    added docker files and logic

--- obj/Release/net8.0/MyDockerTest1.AssemblyInfo.cs
+++ obj/Release/net8.0/MyDockerTest1.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDockerTest1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f70c90c81541047ef9f2d92eca993c1ec907ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDockerTest1")]
